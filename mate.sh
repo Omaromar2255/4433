8f8edce8bcabd463524a8fa2e21543929e54f3e8
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-01-11 14:43:42 -0800

    Update mate.sh

--- mate.sh
+++ mate.sh
@@ -3,11 +3,7 @@ echo "Download windows files"
 echo "===================================="
 wget https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip > /dev/null 2>&1
 unzip chromedriver_linux64.zip  > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3389 &>/dev/null &
 echo Downloading File From akuh.net
-sudo apt update && apt upgrade -y
 sudo apt install firefox -y
 sudo apt install vlc -y
 sudo pip install fake_useragent
@@ -22,4 +18,4 @@ sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon
 sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
 sudo service xrdp start
 wget https://github.com/Omaromar2255/w-colab/edit/main/mountdrive.py
-python ./mountdrive.py
+python 4433-main/mountdrive.py
