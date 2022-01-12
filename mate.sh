f5a42841760d748431c461c053e2ee3f00d106e9
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-01-11 16:30:02 -0800

    yy
    yy

--- mate.sh
+++ mate.sh
@@ -3,7 +3,6 @@ echo "Download ngrok"
 wget https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip > /dev/null 2>&1
 unzip chromedriver_linux64.zip  > /dev/null 2>&1
 echo Downloading File From akuh.net
-sudo apt update && apt upgrade -y
 sudo apt install firefox -y
 sudo apt install vlc -y
 sudo pip install fake_useragent
@@ -13,10 +12,6 @@ sudo apt-get install -y xarchiver
 sudo apt-get install libtxc-dxtn-s2tc0
 sudo apt-get install mesa-utils
 sudo apt-get install winff -y
-sudo apt-get install -y xrdp
-sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
-sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
-sudo service xrdp start
 python 4433-main/mountdrive.py
 ! cd /home/user/
 ! wget -O main.zip https://codeload.github.com/Omaromar2255/4433/zip/refs/heads/main
