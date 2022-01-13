9368102f49fce3a35ed6f337924dbee8a30f3ce3
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-01-13 05:32:01 -0800

    ww
    ww

--- mate.sh
+++ mate.sh
@@ -3,13 +3,9 @@ echo "Download ngrok"
 wget https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip > /dev/null 2>&1
 unzip chromedriver_linux64.zip  > /dev/null 2>&1
 echo Downloading File From akuh.net
-sudo apt install firefox -y
 sudo apt install vlc -y
 sudo pip install fake_useragent
 sudo pip install selenium
-sudo apt install ffmpeg -y
-sudo apt-get install -y xarchiver
-sudo apt-get install libtxc-dxtn-s2tc0
 sudo apt-get install mesa-utils
 sudo apt-get install winff -y
 python 4433-main/mountdrive2.py
