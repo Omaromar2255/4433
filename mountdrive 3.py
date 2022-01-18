fcac4731bbb3e6c7ac1cf028b0f1e66ed12faf9b
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-01-18 18:13:58 +0000

    rr
    rr

---
+++ mountdrive 3.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+import time
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/12.0.0 Mobile/15A5370a Safari/602.1')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://l.facebook.com/l.php?u=https%3A%2F%2Fdailymotion.com%2Fplaylist%2Fx7c2rc%3Ffbclid%3DIwAR2lywnuJPM6PJxXdvnFnLF58rz3jIWyT49wRuMB5WcbHDdgdbeTGw0-oWc&h=AT0jPhdO0EzMxk2o3mIy2wEAHnWz-idBoN8wTUZezFyWQ2jAY3BuLgplZV4YiRNEeKLh9Gorqp3g22OXGtNjjJaSVECqmBHDdiMfnJB2N-SDdpO62ZfNFsWa7VbY22C5gnfc&__tn__=H-R&c[0]=AT3pN4q2JcgBchcnJHyBElDKCtuhwa7ekgxaUYYKCi3W4IqjIfSOCE_bQSbZf2oTdy11NGl_IUKV-t6EXdFxJG6I2aQrkdgv7eV4AGkmzRBONnWp-KcF_ioMXhPdToMOE4sMn8dD23dEIUqg9QcK8gxAGyaFKh-aVyD30NR0A-V1UBWnQnR6umlQVAO_JadwqdbO')
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 10; OnePlus Nord) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://t.co/ZCqZK38WxU')
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 12; SM-G998B Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3396.87 Mobile Safari/537.36')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiph7W445P1AhVpn-AKHWh7C5kQtwJ6BAgDEAI&url=https%3A%2F%2Fwww.dailymotion.com%2Fplaylist%2Fx7c2rc&usg=AOvVaw2jE3x60yyMyTNOXRle8zn_')
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/12.0.0 Mobile/15A5370a Safari/602.1')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://t.co/ZCqZK38WxU')
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/14.0.0 Mobile/15A5370a Safari/602.1')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://l.facebook.com/l.php?u=https%3A%2F%2Fdailymotion.com%2Fplaylist%2Fx7c2rc%3Ffbclid%3DIwAR2lywnuJPM6PJxXdvnFnLF58rz3jIWyT49wRuMB5WcbHDdgdbeTGw0-oWc&h=AT0jPhdO0EzMxk2o3mIy2wEAHnWz-idBoN8wTUZezFyWQ2jAY3BuLgplZV4YiRNEeKLh9Gorqp3g22OXGtNjjJaSVECqmBHDdiMfnJB2N-SDdpO62ZfNFsWa7VbY22C5gnfc&__tn__=H-R&c[0]=AT3pN4q2JcgBchcnJHyBElDKCtuhwa7ekgxaUYYKCi3W4IqjIfSOCE_bQSbZf2oTdy11NGl_IUKV-t6EXdFxJG6I2aQrkdgv7eV4AGkmzRBONnWp-KcF_ioMXhPdToMOE4sMn8dD23dEIUqg9QcK8gxAGyaFKh-aVyD30NR0A-V1UBWnQnR6umlQVAO_JadwqdbO'')
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SM-T970) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3282.137 Safari/537.36')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://t.co/ZCqZK38WxU')
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/14.0 Mobile/15A5341f Safari/604.1')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://t.co/ZCqZK38WxU')
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-F700U/DS Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3396.87 Mobile Safari/537.36')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiph7W445P1AhVpn-AKHWh7C5kQtwJ6BAgDEAI&url=https%3A%2F%2Fwww.dailymotion.com%2Fplaylist%2Fx7c2rc&usg=AOvVaw2jE3x60yyMyTNOXRle8zn_')
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; VOG-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3683.90 Mobile Safari/537.36')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://l.facebook.com/l.php?u=https%3A%2F%2Fdailymotion.com%2Fplaylist%2Fx7c2rc%3Ffbclid%3DIwAR2lywnuJPM6PJxXdvnFnLF58rz3jIWyT49wRuMB5WcbHDdgdbeTGw0-oWc&h=AT0jPhdO0EzMxk2o3mIy2wEAHnWz-idBoN8wTUZezFyWQ2jAY3BuLgplZV4YiRNEeKLh9Gorqp3g22OXGtNjjJaSVECqmBHDdiMfnJB2N-SDdpO62ZfNFsWa7VbY22C5gnfc&__tn__=H-R&c[0]=AT3pN4q2JcgBchcnJHyBElDKCtuhwa7ekgxaUYYKCi3W4IqjIfSOCE_bQSbZf2oTdy11NGl_IUKV-t6EXdFxJG6I2aQrkdgv7eV4AGkmzRBONnWp-KcF_ioMXhPdToMOE4sMn8dD23dEIUqg9QcK8gxAGyaFKh-aVyD30NR0A-V1UBWnQnR6umlQVAO_JadwqdbO'')
+
+time.sleep(4500)
+driver.quit()
