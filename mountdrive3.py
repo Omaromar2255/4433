3fa969c6670ee69f1a18926dcfbfefbf748b4c9d
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-01-27 05:21:18 -0800

    ttt
    yyyy

--- mountdrive3.py
+++ mountdrive3.py
@@ -10,7 +10,7 @@ print(userAgent)
 options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 10; OnePlus Nord) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36')
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
 driver.get('https://t.co/ZCqZK38WxU')
-
+time.sleep(2000)
 options = Options()
 ua = UserAgent()
 userAgent = ua.random
@@ -18,7 +18,7 @@ print(userAgent)
 options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 12; SM-G998B Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3396.87 Mobile Safari/537.36')
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
 driver.get('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiph7W445P1AhVpn-AKHWh7C5kQtwJ6BAgDEAI&url=https%3A%2F%2Fwww.dailymotion.com%2Fplaylist%2Fx7c2rc&usg=AOvVaw2jE3x60yyMyTNOXRle8zn_')
-
+time.sleep(1500)
 options = Options()
 ua = UserAgent()
 userAgent = ua.random
@@ -26,7 +26,7 @@ print(userAgent)
 options.add_argument(f'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/12.0.0 Mobile/15A5370a Safari/602.1')
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
 driver.get('https://t.co/ZCqZK38WxU')
-
+time.sleep(3000)
 
 options = Options()
 ua = UserAgent()
@@ -35,7 +35,7 @@ print(userAgent)
 options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SM-T970) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3282.137 Safari/537.36')
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
 driver.get('https://t.co/ZCqZK38WxU')
-
+time.sleep(3500)
 options = Options()
 ua = UserAgent()
 userAgent = ua.random
@@ -43,7 +43,7 @@ print(userAgent)
 options.add_argument(f'user-agent=Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/14.0 Mobile/15A5341f Safari/604.1')
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
 driver.get('https://t.co/ZCqZK38WxU')
-
+time.sleep(4500)
 options = Options()
 ua = UserAgent()
 userAgent = ua.random
