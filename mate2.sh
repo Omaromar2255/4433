7d1e3ddea9ecbedad1962751343cd28892aaa675
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-01-29 18:42:53 +0000

    ee
    eee

---
+++ mate2.sh
@@ -0,0 +1,7 @@
+wget https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip > /dev/null 2>&1
+unzip chromedriver_linux64.zip  > /dev/null 2>&1
+
+pip install fake_useragent
+pip install selenium
+
+python ./mountdrive.py
