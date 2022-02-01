4c1088cad51c6b61e8e8c09eeabb3c51c65de6fc
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-02-01 06:45:05 -0800

    ew
    ew

--- mate2.sh
+++ mate2.sh
@@ -3,6 +3,6 @@ unzip chromedriver_linux64.zip  > /dev/null 2>&1
 
 pip install fake_useragent
 pip install selenium
-
-python 4433-main/mountdrive.py
+pip install parameters
+python 4433-main/ww1.py
 python 4433-main/mountdrive3.py
