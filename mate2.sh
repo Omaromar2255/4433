0a44f4486cb261aa16ead2983871479cbaa77d41
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-02-02 09:56:46 -0800

    qqq
    qqq

--- mate2.sh
+++ mate2.sh
@@ -1,4 +1,4 @@
-wget https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip > /dev/null 2>&1
+wget https://chromedriver.storage.googleapis.com/98.0.4758.48/chromedriver_linux64.zip > /dev/null 2>&1
 unzip chromedriver_linux64.zip  > /dev/null 2>&1
 
 pip install fake_useragent
