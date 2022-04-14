7008648cc9b285186b2d7f29686fb142f4cd3b58
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-04-14 20:46:28 +0300

    rr
    rrr

--- mate.sh
+++ mate.sh
@@ -1,9 +1,8 @@
 rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 echo "Download ngrok"
-wget https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip > /dev/null 2>&1
+wget https://chromedriver.storage.googleapis.com/100.0.4896.60/chromedriver_linux64.zip > /dev/null 2>&1
 unzip chromedriver_linux64.zip  > /dev/null 2>&1
 echo Downloading File From akuh.net
-sudo apt install vlc -y
 sudo pip install fake_useragent
 sudo pip install selenium
 sudo apt-get install mesa-utils
