3a770e748372decbeeaac14273162139520bead0
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-04-23 21:05:26 +0300

    aa
    ss

--- mountdrive.py
+++ mountdrive.py
@@ -6,6 +6,18 @@ options = Options()
 ua = UserAgent()
 userAgent = ua.random
 print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G980F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3396.87 Mobile Safari/537.36')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=funny+video+china+9&&view=detail&mid=E52EFA59773D13C658A2E52EFA59773D13C658A2&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dfunny%2Bvideo%2Bchina%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(70)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
 options.add_argument(f'user-agent=Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/14.0 Mobile/15A5341f Safari/604.1')
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
 driver.get('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=video&cd=&cad=rja&uact=8&ved=2ahUKEwi5seCjo5H1AhVojIkEHVLrD7oQuAJ6BAgIEAc&url=https%3A%2F%2Fwww.dailymotion.com%2Fvideo%2Fx866yjq&usg=AOvVaw0Bl5GVHofQiIlT0o9qTx5n')
@@ -92,6 +104,216 @@ driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriv
 driver.get('https://t.co/2XpvbW4LN4')
 time.sleep(120)
 
+
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/14.0 Mobile/15A5341f Safari/604.1')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=funny+video+china+8&&view=detail&mid=F3BA1DF6799A355ACD41F3BA1DF6799A355ACD41&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dfunny%2Bvideo%2Bchina%2B8%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(65)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-F700U/DS Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3396.87 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=funny+video+china+7&qpvt=funny+video+china+7&FORM=VDRE')
+time.sleep(80)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G980F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3396.87 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=funny+video+china+6&qpvt=funny+video+china+6&FORM=VDRE')
+time.sleep(92)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4389.105 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=funny+video+china+5&qpvt=funny+video+china+5&FORM=VDRE')
+time.sleep(89)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-F700U/DS Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3396.87 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=funny+video+china+4&qpvt=funny+video+china+4&FORM=VDRE')
+time.sleep(102)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SM-T970) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3282.137 Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=FE903E31046D32613307FE903E31046D32613307&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(180)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/14.0 Mobile/15A5341f Safari/604.1')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=4C3577DBE611347471AD4C3577DBE611347471AD&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(190)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/12.0.0 Mobile/15A5370a Safari/602.1')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=881559A2AEE40E97DBFE881559A2AEE40E97DBFE&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(180)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4389.105 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=EEFFB4949B378BE8B794EEFFB4949B378BE8B794&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(200)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 10; OnePlus Nord) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=F913F68A01C66EEE7B87F913F68A01C66EEE7B87&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(210)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/12.0.0 Mobile/15A5370a Safari/602.1')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=FD97C256FD4D65970018FD97C256FD4D65970018&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(205)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SM-T970) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3282.137 Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=A90BB344B833765C8ABAA90BB344B833765C8ABA&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(270)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4389.105 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=A90BB344B833765C8ABAA90BB344B833765C8ABA&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(200)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 10; OnePlus Nord) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=C62AEAA7EEA40E32BE3DC62AEAA7EEA40E32BE3D&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(230)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SM-T970) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.3282.137 Safari/537.36')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=B3A87153BD0D653265DBB3A87153BD0D653265DB&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(225)
+driver.quit()
+
+
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/12.0.0 Mobile/15A5370a Safari/602.1')
+
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
+driver.get('https://www.bing.com/videos/search?q=XO+Team+-+%22xo+team%22+best+tiktok+compilation+(part+9&&view=detail&mid=A5421CA4AC1FC66DC1B9A5421CA4AC1FC66DC1B9&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3DXO%2BTeam%2B-%2B%2522xo%2Bteam%2522%2Bbest%2Btiktok%2Bcompilation%2B%2528part%2B9%26go%3DSearch%26qs%3Dds%26form%3DQBVR')
+time.sleep(195)
+driver.quit()
+
 driver.quit()
 options = Options()
 ua = UserAgent()
@@ -101,10 +323,4 @@ options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 11; SM-T970) Apple
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'./chromedriver')
 driver.get('https://t.co/ZCqZK38WxU')
 time.sleep(5000)
-driver.quit()
-
-
-
-
-time.sleep(120)
 driver.quit()
