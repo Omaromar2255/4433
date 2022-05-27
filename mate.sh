a843ef8424ded5055175f16d7c769878b71cc515
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-05-27 19:24:12 +0300

    xx
    xxx

--- mate.sh
+++ mate.sh
@@ -1,11 +1,16 @@
 rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 echo "Download ngrok"
-wget https://chromedriver.storage.googleapis.com/100.0.4896.60/chromedriver_linux64.zip > /dev/null 2>&1
+wgethttps://chromedriver.storage.googleapis.com/101.0.4951.41/chromedriver_linux64.zip > /dev/null 2>&1
 unzip chromedriver_linux64.zip  > /dev/null 2>&1
 echo Downloading File From akuh.net
 pip install fake_useragent
 pip install selenium
 apt-get install mesa-utils
 python 4433-main/mountdrive2.py
+python 4433-main/mountdrive.py
 python 4433-main/mountdrive2.py
+python 4433-main/mountdrive.py
+python 4433-main/mountdrive2.py
+python 4433-main/mountdrive2.py
+python 4433-main/mountdrive.py
 python 4433-main/mountdrive.py
