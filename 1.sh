d6dd5bbf57d206744c5fa1e5bcd9faf64304be67
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-11 21:06:32 +0300

    zz
    zz

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-./ngrok authtoken 1zkBF3syJAzN0RFODJEAOXgnSXj_2gSkHzsqoTKcqjFWj7z1L
+./ngrok authtoken 29kumLZKxG3YxiTf127Fapj47a1_5oeR2sT6j2jEmghSkmrUA
 ./ngrok tcp --region us 3389
 echo "======================="
