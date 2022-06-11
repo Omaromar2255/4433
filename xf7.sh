80c152344716b8da50f63dd73cce2536e88569e2
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-11 23:54:54 +0300

    xx
    xx

---
+++ xf7.sh
@@ -0,0 +1,40 @@
+echo "Download ngrok"
+chmod +x 444-main/ngrok7.sh && ./444-main/ngrok7.sh
+echo Downloading File From akuh.net
+echo "===================================="
+echo "Wait 2 Minutes"
+echo "This is only for education"
+echo "Other interesting tutorials visit akuh.net"
+echo "===================================="
+sudo apt-get update > /dev/null 2>&1
+echo "===================================="
+echo "Install Firefox"
+echo "===================================="
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  > /dev/null 2>&1
+sudo apt install ./google-chrome-stable_current_amd64.deb  > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install vlc -y  > /dev/null 2>&1
+sudo pip install fake_useragent  > /dev/null 2>&1
+sudo pip install selenium  > /dev/null 2>&1
+sudo apt-get install -y xarchiver  > /dev/null 2>&1
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt-get install xfce4 xfce4-goodies > /dev/null 2>&1
+sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
+sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
+sudo systemctl disable lightdm.service > /dev/null 2>&1
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "More  Free VPS akuh.net"
+echo "Don't close this tab RDP runs 12 hours"
+echo "Keep support akuh.net thank you"
+echo "===================================="
+sleep 43210
