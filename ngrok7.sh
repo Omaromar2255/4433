3f9c9125fbbd7739455897be85377ce958a6755e
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-12 00:22:41 +0300

    77
    66

---
+++ ngrok7.sh
@@ -0,0 +1,19 @@
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+rm -rf ngrok ngrok.zip ngrok2.sh > /dev/null 2>&1
+chmod +x 4433-main/ng7.sh
+./4433-main/ng7.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+./ngrok tcp --region us 3389 &>/dev/null &
+echo "==
