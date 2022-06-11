f2e07fa7b5b5db68b4d880902ab9c0de43101c9b
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-12 00:00:54 +0300

    xx
    xx

--- ngrok2.sh
+++ ngrok2.sh
@@ -2,7 +2,7 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 rm -rf ngrok ngrok.zip ngrok2.sh > /dev/null 2>&1
-chmod +x 4433-main/ng.sh
+chmod +x 4433-main/ng2.sh
 ./4433-main/ng2.sh
 clear
 echo "======================="
@@ -18,5 +18,3 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3389 &>/dev/null &
 echo "==
-© 2022 GitHub, Inc.
-Terms
