64c0079132cfa1d01d219f03f21ec454337f5fa1
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-12 02:09:33 +0300

    xx
    xx

--- ngrok7.sh
+++ ngrok7.sh
@@ -2,8 +2,8 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 rm -rf ngrok ngrok.zip ngrok2.sh > /dev/null 2>&1
-chmod +x 4433-main/ng7.sh
-./4433-main/ng7.sh
+chmod +x 4433-main/ng5.sh
+./4433-main/ng5.sh
 clear
 echo "======================="
 echo choose ngrok region
@@ -15,5 +15,5 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-./ngrok tcp --region us 3389 > /dev/null 2>&1
+./ngrok tcp --region us 3389 &>/dev/null &
 echo "==
