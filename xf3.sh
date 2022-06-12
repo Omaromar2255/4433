eda96182fad3aa19473850f77cd01945d29fdeed
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-13 00:05:43 +0300

    xx
    xx

--- xf3.sh
+++ xf3.sh
@@ -6,28 +6,8 @@ echo "Wait 2 Minutes"
 echo "This is only for education"
 echo "Other interesting tutorials visit akuh.net"
 echo "===================================="
-sudo apt-get update > /dev/null 2>&1
-echo "===================================="
-echo "Install Firefox"
-echo "===================================="
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  > /dev/null 2>&1
-sudo apt install ./google-chrome-stable_current_amd64.deb  > /dev/null 2>&1
-sudo apt update > /dev/null 2>&1
-sudo apt install vlc -y  > /dev/null 2>&1
-sudo pip install fake_useragent  > /dev/null 2>&1
-sudo pip install selenium  > /dev/null 2>&1
-sudo apt-get install -y xarchiver  > /dev/null 2>&1
-echo "===================================="
-echo "Install RDP"
 echo "===================================="
 sudo apt install -y xrdp > /dev/null 2>&1
-sudo apt-get install xfce4 xfce4-goodies > /dev/null 2>&1
-sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
-sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
-chmod +x 4433-main/mate2.sh > /dev/null 2>&1
-cp -r 4433-main/mate2.sh /home/22/mate2.sh > /dev/null 2>&1
-sudo systemctl disable lightdm.service > /dev/null 2>&1
-echo "===================================="
 echo "Start RDP"
 echo "===================================="
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
