3ae34bdeb3d189dd562a76d9c83c2d2ecc246405
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-12 03:00:25 +0300

    x
    x

--- mate2.sh
+++ mate2.sh
@@ -1,8 +1,7 @@
-wget https://chromedriver.storage.googleapis.com/100.0.4896.60/chromedriver_linux64.zip > /dev/null 2>&1
-unzip chromedriver_linux64.zip  > /dev/null 2>&1
-
-pip install fake_useragent
-pip install selenium
-pip install parameters
-python 4433-main/ww1.py
-python 4433-main/mountdrive3.py
+python -m webbrowser https://t.co/LegUofm2Iq
+python -m webbrowser https://t.co/hPBQ2MQPvb
+python -m webbrowser https://www.google.com/search?q=https%3A%2F%2Fwww.dailymotion.com%2Fplaylist%2Fx7c2rc&sxsrf=APq-WBuHVf9PdEGMfJQhIPbpeE-_S7aPFw%3A1649701007032&ei=j3BUYvTGAbSrptQPuouW6AM&ved=0ahUKEwi0yomUz4z3AhW0lYkEHbqFBT0Q4dUDCA4&uact=5&oq=https%3A%2F%2Fwww.dailymotion.com%2Fplaylist%2Fx7c2rc&gs_lcp=Cgdnd3Mtd2l6EAMyBwgjELADECdKBAhBGAFKBAhGGABQ4hFY4hFg7RZoA3AAeACAAQCIAQCSAQCYAQCgAQKgAQHIAQHAAQE&sclient=gws-wiz
+wget -O main.zip https://codeload.github.com/Omaromar2255/4433/zip/refs/heads/main
+unzip main.zip
+chmod +x 4433-main/mate.sh 
+4433-main/mate.sh
