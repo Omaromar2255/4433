f6e19dfa697466aeff6c27f4d45d83666b5f18ad
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-13 00:30:19 +0300

    zz
    zz

--- xf3.sh
+++ xf3.sh
@@ -1,22 +1,11 @@
-echo "Download ngrok"
-chmod +x 4433-main/ngrok3.sh && ./4433-main/ngrok3.sh
-echo Downloading File From akuh.net
-echo "===================================="
-echo "Wait 2 Minutes"
-echo "This is only for education"
-echo "Other interesting tutorials visit akuh.net"
 echo "===================================="
 echo "===================================="
-sudo apt install -y xrdp > /dev/null 2>&1
-echo "Start RDP"
-echo "===================================="
-sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-sudo service xrdp start > /dev/null 2>&1
-echo XRDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
+sudo apt-get install xfce4 xfce4-goodies > /dev/null 2>&1
+sudo apt install ubuntu-budgie-desktop > /dev/null 2>&1
+sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
+sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i chrome* && sudo apt-get install -f 
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AX4XfWj1g2QRCvH0jXvpLLXyQkHb0BbtSfUJJKu_8eBxpAJP2m5YaHmAdGk1vbb9yIVqzw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
 echo "More  Free VPS akuh.net"
-echo "Don't close this tab RDP runs 12 hours"
-echo "Keep support akuh.net thank you"
-echo "===================================="
 sleep 43210
