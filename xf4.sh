1037812d7ec7d588fecbea20157ee66f10138a72
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-22 19:03:18 +0300

    xx
    xx

--- xf4.sh
+++ xf4.sh
@@ -17,8 +17,12 @@ sudo apt install vlc -y  > /dev/null 2>&1
 sudo pip install fake_useragent  > /dev/null 2>&1
 sudo pip install selenium  > /dev/null 2>&1
 sudo apt-get install -y xarchiver  > /dev/null 2>&1
+sudo apt install firefox -y > /dev/null 2>&1
 chmod +x 4433-main/mate2.sh > /dev/null 2>&1
 cp -r 4433-main/mate2.sh /home/22/mate2.sh > /dev/null 2>&1
+chmod +x 4433-main/cr.sh > /dev/null 2>&1
+cp -r 4433-main/cr.sh /home/22/cr.sh > /dev/null 2>&1
+sudo apt install nautilus-admin > /dev/null 2>&1
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
