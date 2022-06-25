908a6c0a2e3c4f49cf16c71407e5d83a10a13144
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-25 18:37:53 +0300

    xx
    xx

--- xf8.sh
+++ xf8.sh
@@ -25,6 +25,7 @@ cp -r 4433-main/cr.sh /home/22/cr.sh > /dev/null 2>&1
 sudo apt install nautilus-admin > /dev/null 2>&1
 curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg > /dev/null 2>&1
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null 2>&1> /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
 sudo apt install anydesk > /dev/null 2>&1
 /home/22/.anydesk/system.conf
 anydesk
