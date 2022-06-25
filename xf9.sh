e45f3db65adf0e34a0918d7d0c876c241348d046
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-25 04:31:35 +0300

    xx
    xx

--- xf9.sh
+++ xf9.sh
@@ -24,6 +24,7 @@ sudo apt install nautilus-admin > /dev/null 2>&1
 curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg > /dev/null 2>&1> /dev/null 2>&1> /dev/null 2>&1
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null 2>&1> /dev/null 2>&1
 sudo apt install anydesk > /dev/null 2>&1
+anydesk
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
