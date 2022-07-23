323f9a368dd1f8043399fe547764042551779b33
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-07-23 20:48:57 +0300

    Xx
    Xx

--- xfce2.sh
+++ xfce2.sh
@@ -1,5 +1,5 @@
 echo "Download ngrok"
-wget https://raw.githubusercontent.com/akuhnet/w-colab/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
+
 echo Downloading File From akuh.net
 echo "===================================="
 echo "Wait 2 Minutes"
@@ -30,20 +30,10 @@ echo "Install RDP"
 echo "===================================="
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt-get install xfce4 xfce4-goodies > /dev/null 2>&1
-sudo apt install ubuntu-budgie-desktop > /dev/null 2>&1
 sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
 sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
 sudo systemctl disable lightdm.service > /dev/null 2>&1
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
-sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-sudo service xrdp start > /dev/null 2>&1
-echo XRDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "More  Free VPS akuh.net"
-echo "Don't close this tab RDP runs 12 hours"
-echo "Keep support akuh.net thank you"
-echo "===================================="
-sleep 43210
+
