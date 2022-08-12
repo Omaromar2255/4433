fd7ba2a7a9473cacc797a1d91f2197fa98bdf108
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-08-12 11:02:29 -0700

    xx
    xx

--- xf4.sh
+++ xf4.sh
@@ -6,31 +6,34 @@ echo "Wait 2 Minutes"
 echo "This is only for education"
 echo "Other interesting tutorials visit akuh.net"
 echo "===================================="
-apt-get update
-wget -O w2019.gz https://go.aank.me/win/W2019-aank.gz
-gunzip w2019.gz
-echo "Wait..."
-echo "I m Working Now.."
-mv w2019 w2019.img
-echo Downloading files from aank.me
-apt-get install qemu-system-x86 -y
-echo "Wait..."
-echo "Starting Windows"
-qemu-system-x86_64 -hda w2019.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+apt update
+apt install screen -y
+apt install sudo -y
+sudo adduser omar --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "aank:omar" | sudo chpasswd
+sudo usermod -aG sudo,adm aank
 clear
+echo "Install XFCE"
+echo "===================================="
+sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=======25%"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "=============50%"
+sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal
+echo "=================70%"
+echo xfce4-session >~/.xsession
+echo "=====================90%"
+sudo service xrdp start
+clear
+echo "=======================100%"
+echo "===================================="
+echo "Username : aank"
+echo "Password : LinggaHosting"
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "Username: Administrator"
-echo "Password: Lingg@H0sting"
-echo "===================================="
-echo "===================================="
-echo "Don't closse this Tab"
-echo "Wait 1 - 3 minut for finishing bot"
-echo "RDP run up to 50 hours"
-echo "Support YT Channel-> Aank is ME, thankyou"
-echo "Link-> https://aank.me/Youtube"
-echo "===================================="
+rm -rf ngrok.zip xrdp.sh
 b='\033[1m'
 r='\E[31m'
 g='\E[32m'
