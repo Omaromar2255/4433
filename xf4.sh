6c8414cddf6f284cca89c2e59e244af49bae61e4
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-08-12 13:48:13 -0700

    Update xf4.sh

--- xf4.sh
+++ xf4.sh
@@ -8,10 +8,8 @@ echo "root:222" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 useradd -m 2211
 sudo adduser 2211 
-adduser 2211 sudo
 echo "2211:222" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo usermod -aG sudo,adm 2211
 echo "Download ngrok"
 chmod +x 4433-main/ngrok4.sh && ./4433-main/ngrok4.sh
 
