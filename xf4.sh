55cc44133f4ffb21bff2651668d7b4b3c8f652ee
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-08-12 11:54:22 -0700

    Update xf4.sh

--- xf4.sh
+++ xf4.sh
@@ -10,42 +10,23 @@ echo Downloading File From akuh.net
 clear
 echo "Install XFCE"
 echo "===================================="
-sudo apt install -y xfce4 xfce4-goodies 
-echo "=======25%"
-sudo apt install firefox -y > /dev/null 2>&1
-echo "=============50%"
-sudo apt-get install -y xrdp > /dev/null 2>&1
-sudo apt-get install -y xfce4-terminal
-echo "=================70%"
-echo xfce4-session >~/.xsession
-echo "=====================90%"
-sudo service xrdp start
-clear
-echo "=======================100%"
+echo "Install RDP"
 echo "===================================="
-echo "Username : aank"
-echo "Password : LinggaHosting"
-echo RDP Address:
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt-get install xfce4 xfce4-goodies 
+sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
+sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
+sudo systemctl disable lightdm.service > /dev/null 2>&1
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-rm -rf ngrok.zip xrdp.sh
-b='\033[1m'
-r='\E[31m'
-g='\E[32m'
-c='\E[36m'
-endc='\E[0m'
-enda='\033[0m'
-# Branding
-
-printf """$c$b
- 
-██╗     ██╗███╗   ██╗ ██████╗  ██████╗  █████╗ ██╗  ██╗ ██████╗ ███████╗████████╗██╗███╗   ██╗ ██████╗ 
-██║     ██║████╗  ██║██╔════╝ ██╔════╝ ██╔══██╗██║  ██║██╔═══██╗██╔════╝╚══██╔══╝██║████╗  ██║██╔════╝ 
-██║     ██║██╔██╗ ██║██║  ███╗██║  ███╗███████║███████║██║   ██║███████╗   ██║   ██║██╔██╗ ██║██║  ███╗
-██║     ██║██║╚██╗██║██║   ██║██║   ██║██╔══██║██╔══██║██║   ██║╚════██║   ██║   ██║██║╚██╗██║██║   ██║
-███████╗██║██║ ╚████║╚██████╔╝╚██████╔╝██║  ██║██║  ██║╚██████╔╝███████║   ██║   ██║██║ ╚████║╚██████╔╝
-╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝
-    $r  Support YT Channel-> Aank is ME © 2022 $c https://aank.me/Youtube 
-          
-$endc$enda""";
-sleep 43200
+echo "More  Free VPS akuh.net"
+echo "Don't close this tab RDP runs 12 hours"
+echo "Keep support akuh.net thank you"
+echo "===================================="
+sleep 43210
