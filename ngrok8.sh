6fcae0423a804d4da23d52bb7648578ff992135c
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2023-02-03 22:00:05 +0300

    Update ngrok8.sh

--- ngrok8.sh
+++ ngrok8.sh
@@ -15,5 +15,5 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-./ngrok tcp --region us 3389 &>/dev/null &
+./ngrok tcp --region eu 3389 &>/dev/null &
 echo "==
