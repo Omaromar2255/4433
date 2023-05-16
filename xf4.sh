58991a14a512a985ee10ed6d5e7959d189ad7c90
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2023-05-17 01:51:55 +0300

    Update xf4.sh

--- xf4.sh
+++ xf4.sh
@@ -4,11 +4,11 @@ apt install sudo -y
 useradd -m root
 sudo adduser root 
 adduser root sudo
-echo "root:222" | sudo chpasswd
+echo "root:root" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-useradd -m 2211
-adduser 2211 sudo
-echo "2211:222" | sudo chpasswd
+useradd -m www
+adduser www sudo
+echo "www:www" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 echo "Download ngrok"
 chmod +x 4433-main/ngrok4.sh && ./4433-main/ngrok4.sh
@@ -17,21 +17,25 @@ sudo apt install vlc -y
 sudo apt-get install -y xarchiver  > /dev/null 2>&1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  > /dev/null 2>&1
 sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt install ffmpeg -y  
+curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg 
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list 
+sudo apt update 
+sudo apt install anydesk 
+anydesk 
 echo Downloading File From akuh.net
 clear
 echo "Install RDP"
 echo "===================================="
 sudo apt install -y xrdp 
 sudo apt-get install xfce4 xfce4-goodies 
-sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
-sudo apt install --assume-yes xscreensaver > /dev/null 2>&1
-sudo systemctl disable lightdm.service > /dev/null 2>&1
+sudo apt remove --assume-yes gnome-terminal
+sudo apt install --assume-yes xscreensaver 
+sudo systemctl disable lightdm.service 
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
-sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-sudo service xrdp start > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh >
+sudo service xrdp start 
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
